aba3f73257c695d76311c306315777dc235997e3
Johannes Engels <post@johannesengels.de>
2020-10-12 13:39:20 +0200

    Replies weiter ausgearbeitet. Retweets werden vermutlich nicht beachtet, Fokus komplett auf replies -> to-do: Nachrichtenanalyse der Accounts mit meisten Interaktionen, der Accounts im Zentrum der Analysen, evtl Betrachtung individueller viraler Tweets innerhalb des Netzwerkes.

--- Code - Netzwerk-ger.R
+++ Code - Netzwerk-ger.R
@@ -39,7 +39,7 @@ sum(tweets$retweet_count[tweets$tweetid %in% retweets$retweet_tweetid])
 summary(tweets$retweet_count[tweets$tweetid %in% retweets$retweet_tweetid])
 # 184.000 einzigartige Tweets von 1734 innerhalb des Datensets erhielten 618.000 Retweets von den hier vorhandenen Accounts - bzw. 10% ihrer Gesamt-Retweets, und das obwohl der Reichweitenstärkste Tweet von 123.617 Accounts Retweetet wurde.
 
-# Reply - Analysen ----
+# Reply - Grobe Analysen ----
 rpl_df <- data.frame(from = as.character(), to = as.character(), occurances = as.integer())
 
 for(i in 1:nrow(replies)) {
@@ -90,20 +90,81 @@ for(i in 1:nrow(rpl_dst)) {
   rpl_dst$in.data[i] <- ifelse(rpl_dst$user[i] %in% rpl_src$user, T, F)
 }
 
+# Reply - Wer antortet ----
+
 table(rpl_src$replies)
 table(rpl_dst$occurance)
 # Deutliche Unterschiede in der Anzahl an Interaktionen mit Accounts. Während sich bei den Ziel-Accounts, auf die geantwortet wurde, ein relativ gleichförmiger Anstieg in der Antwortzahl erkennen lässt, dominiert bei den Antwort-Accounts ein einzelner Account mit 30k Antworten - mehr als das 7fache des Accounts mit den zweitmeisten Antworten
+
+g <- graph.data.frame(rpl_df)
+degcent.g_in <- degree(g, normalized = T, mode = "in")
+degcent.g_out <- degree(g, normalized = T, mode = "out")
+
+head(sort(degcent.g_in, decreasing = T), 20)
+head(sort(degcent.g_out, decreasing = T), 20)
+# Während sich bei den in-degree-Accounts kein deutlicher Unterschied in der Zentralität zeigt, so dominiert bei den out-degrees der bereits im vorherigen Schritt als bedeutend identifizierte Account, mit einem fast zehnmal so hohen Wert wie Platz 2.
+
 table(rpl_df$occurances[rpl_df$from == rpl_src$user[rpl_src$replies == max(rpl_src$replies)]])
 #Dieser dominierende Account setzt dabei hauptsächlich auf ANtworten an viele unterschiedliche Accounts, nur verhältnismäßig wenige finden sich mehrfach in den Daten.
+gdom.answered <- rpl_df %>% filter(from == rpl_src$user[rpl_src$replies == max(rpl_src$replies)])
+table(gdom.answered$to %in% users$userid)
 for.gdom <- rpl_df %>% filter(from == rpl_src$user[rpl_src$replies == max(rpl_src$replies)]) %>%
   filter(occurances >= 6)
 g.dom <- graph.data.frame(for.gdom)
 V(g.dom)$color <- ifelse(V(g.dom)$name %in% rpl_src$user, "orange", "light blue")
 plot.igraph(g.dom, 
             edge.width=E(g.dom)$occurances /4)
-# Accounts, denen mehr als 5 mal geantwortet wurde, finden sich alle nicht in den Daten.
+# Keiner der Accounts, denen dieser Spambot geantwortet hat, finden sich in diesem Datensatz
+
+dom_out.id <- users$userid[which(substr(users$userid, 1, 8) == 
+                        substr(names(head(sort(degcent.g_out, decreasing = T), 1)), 1, 8))]
+dom_out.twe <- tweets %>% filter(userid == dom_out.id)
+
+head(dom_out.twe$tweet_text)
+min(dom_out.twe$tweet_time); max(dom_out.twe$tweet_time)
+table(!(is.na(dom_out.twe$in_reply_to_tweetid)))
+table(grepl("workout|exercise", tolower(dom_out.twe$tweet_text)))
+head(dom_out.twe$tweet_text[!(grepl("workout|exercise", tolower(dom_out.twe$tweet_text)))],10)
+table(grepl("@", dom_out.twe$tweet_text))
+# Der dominante Account hat in anderthalb Monaten Aktivität knapp 65.000 Tweets abgesetzt, taggt in quasi jedem Tweet andere Nutzer, gut die Hälfte seiner Tweets sind Antworten auf andere Nutzer und 41% seiner Tweets beinhalten die Worte "workout" bzw. "exercise". Es handelt sich hier also mit sehr großer Wahrscheinlichkeit um einen Spam-Bot.
+rm(dom_out.twe, dom_out.id, g, degcent.g_in, degcent.g_out, for.gdom, g.dom, gdom.answered)
+
+
+
+# Reply - Wem wird geantwortet ----
+i <- 240
+# Maximal beantwortete Accounts
+table(rpl_dst$occurance)
+table(rpl_dst$user[rpl_dst$occurance == 3252] %in% rpl_src$user)
+# Dominant viele Accounts mit nur einer Antwort (verm. Spam an Accounts außerhalb der Daten, da zu 98,5% Ziel-Accounts nicht im Datensatz), teilweise Accounts mit mehreren Tausend Antworten, aber keine deutlich abgegrenzten Spitzen -> Analyse der top beantworteten Accounts für mehr Klarheit: Cutoff 500 Antworten, 38 Accounts)
+dat <- data.frame(occur = as.vector(table(rpl_dst$occurance)), 
+                  interact = as.integer(names(table(rpl_dst$occurance))), 
+                  false = 0, true = 0)
+for(i in 1:nrow(dat)){
+  ft <- table(rpl_dst$user[rpl_dst$occurance == dat$interact[i]] %in% rpl_src$user)
+  names(ft)
+  if(length(ft) == 2){
+    dat[i, 3:4] <- ft
+  } else {
+    if(names(ft) == "FALSE"){
+      dat[i, 3] <- ft
+    } else {
+      dat[i, 4] <- ft
+    }
+  }
+}
+dat$out_perc <- dat$false / (dat$false + dat$true) * 100
+my_breaks <- c(2, 10, 50, 250, 1000, 10000)
+ggplot(dat, aes(x = interact, y = out_perc)) + geom_point(aes(color = occur)) + scale_x_log10() +
+  scale_color_gradient(name = "Anzahl an\nAccounts", trans = "log", breaks = my_breaks, low = "red", high = "green") +
+  labs(title = "Verteilung der beantworteten Accounts nach Interaktionszahl",
+       subtitle = 'Interaktionen mit "echten" Accounts, die nicht Teil des Datensets sind',
+       x = "Anzahl an Interaktionen",
+       y = "Prozentsatz an Accounts nicht im Datensatz")
+# Accounts mit unter ~ 70 Antworten finden sich deutlich öfter nicht im Datensatz, Accounts mit >1000 Antworten finden sich deutlich öfter innerhalb des Datensatzes.
 
 for.gmaxrep <- rpl_df %>% filter(to %in% rpl_dst$user[rpl_dst$occurance >= 500])
+table(unique(for.gmaxrep$to) %in% rpl_df$from)
 
 g.maxrep <- graph.data.frame(for.gmaxrep, directed = T)
 g.maxrep
@@ -114,63 +175,15 @@ plot.igraph(g.maxrep, layout = l * 1000, vertex.size = 2, vertex.label.cex = 0.5
             edge.width=sqrt(E(g.maxrep)$occurances)/2, edge.arrow.size = 0.25)
 
 # Obwohl 95% der Antworten an Accounts außerhalb des Netzwerkes gerichtet sind, finden sich in den Accounts mit den meisten Antworten deutliche Muster:
-#   -> Die 10 Accounts außerhalb des Netzwerkes, die hier auftauchen, sind mit je ein bis drei Netzwerk-Accounts verbunden, die ihnen antworten
+#   -> Nur 11 der 38 Accounts mit den meisten Replys stammen von außerhalb des Datensatzes
+#   -> Die Accounts außerhalb des Netzwerkes, die hier auftauchen, sind mit je ein bis drei Netzwerk-Accounts verbunden, die ihnen antworten
 #   -> mehrere dezentrale Netzwerke finden sich in den Daten, die sich relativ gleichmäßig gegenseitig antworten
 #   -> drei Accounts, der sich dominant bzw. ausschließlich selbst antwortet. Einer ist gleichzeitig Zentrum eines der
 #   -> sechs Cluster, in denen sich Accounts um einen zentralen Account scharen und diesem Antworten. Alle diese zentralen Accounts befinden sich in den Daten, und die Netzwerkgrößen schwanken deutlich (6 bis >100 Antwort-Accounts.)
 #   -> zwei der größten Cluster sind direkt über eine vergleichsweise kleine Interaktion miteinander verbunden, während zwei mittlere und ein kleiner durch eine Antort-"Achse" niteinander verbunden sind, die die stärksten Interaktionen zwischen zwei Accounts aufweist und mehrere der dezentralen Cluster mit einschließt.
 
-
-
-# Großteil der Verbindungen ziwschen Accounts nur einmalig und viele Accounts mit nur einer einzigen Verbindung, hier jedoch mehrfach auftretende Verbindungen interessant. Erste Erkenntnis - Maxima: 2011-faches Auftreten der selben Vrbindung zweier Accounts, ein einzelner Account mit 30000 replies in den Daten.
-g.mid <- graph.data.frame(rpl_df[rpl_df$from %in% rpl_sum$users[rpl_sum$replies > 1], ], directed = T)
-g.mid
-deg.dist <- degree.distribution(g.mid)
-deg.dist # Große Verteilung der einzelnen Gradmengen, weiteres Filtern notwendig.
-g.mid <- graph.data.frame(rpl_df[rpl_df$from %in% rpl_sum$replies[rpl_sum$replies > 1 & rpl_sum$replies < 16], ]
-                          , directed = T)
-barplot(degree.distribution(g.mid))
-# Betracht
-
-# Accounts mit bis zu 15 degrees tauchen häufiger in den Daten auf. Jenseits der 15 degrees finden sich spradisch einzelne Accounts mitextremen Werten.
-g.max <- delete.vertices(g, which(degree(g) < 15))
-g.max
-barplot(degree.distribution(g.max))
-plot(g.max)
-# 270 Accounts mit mehr als 15 Antworten finden sich in den Daten.
-V(g.max)$color <- ifelse(grepl("hash$", V(g.max)$name), "orange", "blue")
-plot(g.max,
-     layout= layout_nicely(g.max), 
-     vertex.size=degree(g.max) * 2,
-     vertex.label.cex = 0.25,
-     edge.width=E(g.max)$occurances / 100)
-
-
-
-
-# Retweet - Analysen ----
-rtw_df <- data.frame(from = retweets$userid, to = retweets$retweet_userid, 
-                     retweetid = retweets$retweet_tweetid, tweetid = retweets$tweetid, 
-                     rtfollowers = retweets$follower_count, tfollowers = numeric(length = nrow(retweets)), tretweets = numeric(length = nrow(retweets)))
-
-rtw_log <- rtw_df$to %in% users$userid
-for(i in 1:nrow(rtw_df)){
-  if(rtw_log[i] ==T){
-    rtw_df$tfollowers[i] <- users$follower_count[which(users$userid == rtw_df$to[i])]
-  } else{
-    rtw_df$tfollowers[i] <- NA
-  }
-}
-rtw_src <- tweets %>% filter(tweetid %in% rtw_df$retweetid) %>% select(retweet_tweetid, retweet_count)
-for(i in 1:100){
-  if(rtw_log[i] ==T){
-    rtw_df$treweets[i] <- rtw_src$retweet_count[which(rtw_src$tweetid == rtw_df$retweetid[i])]
-  } else{
-    rtw_df$treweets[i] <- NA
-  }
-}
-
-write_csv(rtw_df, file.path("Hausarbeit/retweet_df.csv"), na = "NA", append = FALSE, col_names = T, quote_escape = "double")
-
-
-
+# Accounts in der Mitte der großen Cluster:
+degcent.gmaxr_in <- degree(g.maxrep, normalized = T, mode = "in")
+degcent.gmaxr_out <- degree(g.maxrep, normalized = T, mode = "out")
+head(sort(degcent.gmaxr_in, decreasing = T), 20)
+head(sort(degcent.gmaxr_out, decreasing = T), 20)
